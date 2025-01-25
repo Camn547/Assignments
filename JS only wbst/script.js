a00d8790bbcbda92692bfdf31f052be444f8576a
Camn547 <Camomichael1@icloud.com>
2025-01-24 22:46:04 -0700

    h

--- JS only wbst/script.js
+++ JS only wbst/script.js
@@ -17,12 +17,12 @@ const createAndAppend = (tag, parent, attributes={}, content="")=>{
 
 const header = createAndAppend("header", document.body, {class:"site-header"});
     const headimg = createAndAppend("img",header,{src:"https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/391540/header.jpg?t=1579096091", alt:"undertale", class:"undertale"})
-    const headimg2 = createAndAppend("img",header,{src:"./What-type-of-human-soul-from-undertale-have-you-g-2-8394-1456540382-13_dblbig.webp", alt:"undertale", class:"hearts2"})
-    const headimg3 = createAndAppend("img",header,{src:"./What-type-of-human-soul-from-undertale-have-you-g-2-8394-1456540382-13_dblbig.webp", alt:"undertale", class:"hearts"})
-    const annDog = createAndAppend("img", header,{src:"./Annoying_Dog_sprite.webp", alt:"undertale", class:"annoying"})
-    const funny = createAndAppend("img", header, {src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeuMGmMaEphoSfu4sGOW9w0PzwCpfxmo9L1A&s", alt:"funny", class:"funny"})
-    const sleepDog = createAndAppend("img", header, {src:"./Annoying_Dog_overworld_sleeping.webp", alt: "he sleepy", class:"Sleep"})
-    const funny2 = createAndAppend("img", header, {src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeuMGmMaEphoSfu4sGOW9w0PzwCpfxmo9L1A&s", alt:"funny", class:"funny2"})
+    const headimg2 = createAndAppend("img",header,{src:"./images/What-type-of-human-soul-from-undertale-have-you-g-2-8394-1456540382-13_dblbig.webp", alt:"undertale", class:"hearts2"})
+    const headimg3 = createAndAppend("img",header,{src:"./images/What-type-of-human-soul-from-undertale-have-you-g-2-8394-1456540382-13_dblbig.webp", alt:"undertale", class:"hearts"})
+    const annDog = createAndAppend("img", header,{src:"./images/Annoying_Dog_sprite.webp", alt:"undertale", class:"annoying"})
+    const funny = createAndAppend("img", header, {src:"./images/black.png", alt:"funny", class:"funny"})
+    const sleepDog = createAndAppend("img", header, {src:"./images/Annoying_Dog_overworld_sleeping.webp", alt: "he sleepy", class:"Sleep"})
+    const funny2 = createAndAppend("img", header, {src:"./images/black.png", alt:"funny", class:"funny2"})
 ;
 const navbar = createAndAppend("navbar", document.body, {class:"navbar"});
     let link = document.createElement('a');
@@ -41,51 +41,165 @@ const navbar = createAndAppend("navbar", document.body, {class:"navbar"});
     navbar.appendChild(link3);
 
     let link4 = document.createElement('a');
-    link4.href = '#';
+    link4.href = 'https://youtu.be/LZqQLXlrhI4?si=XcemOz5-CgCm_H0g';
     link4.innerHTML = "Toby";
     navbar.appendChild(link4);
 ;
 
-
-
-
-const section = document.createElement('section');
-for (let i =0; i <= 7; i++){
+const main = createAndAppend("main", document.body)
     const article = document.createElement('article');
+    const img = document.createElement('img');
+    img.src = './images/undertale.webp';
     const content = document.createElement('p');
-    content.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet, elit id finibus semper, felis mauris egestas nisi, non vestibulum nisi arcu in massa. Morbi dictum pulvinar quam ac mattis. Nulla quis erat eu metus dictum placerat. Integer gravida volutpat dictum. Curabitur maximus porta urna, quis accumsan enim porttitor in."
+    content.innerHTML = "Undertale is an amazing indie game created by Toby Fox in 2015, The player controls a child who has fallen into the Underground: a large, secluded region under the surface of the Earth, separated by a magical barrier. The player meets various monsters during the journey back to the surface, some of which may engage in combat."
+    const content2 = document.createElement('p');
+    const space = document.createElement('br')
+    
+    content2.innerHTML = "Undertale is rated E10+ so it is able to be enjoyed by teens."
+    const content3 = document.createElement('p')
+    content3.innerHTML = "It's story and characters are very loveable and"
+    const content4 = document.createElement('p')
+    content4.innerHTML = "memorable, Undertale is highly reccomended among others"
+    const content5 = document.createElement('p')
+    const space2 = document.createElement('br')
+    content5.innerHTML = "Undertale scored 10/10 on IGN, 92% on Metacritic and 10/10"
+    const content6 = document.createElement('p')
+    content6.innerHTML = "on steam. A 'sequal' game is being worked on, Deltarune."
+    article.appendChild(img);
     article.appendChild(content);
-    section.appendChild(article);
-}
-document.body.appendChild(section);
+    article.appendChild(space);
+    article.appendChild(content2);
+    article.appendChild(content3);
+    article.appendChild(content4);
+    article.appendChild(space2);
+    article.appendChild(content5);
+    article.appendChild(content6);
+    main.appendChild(article);
+;
 
+
+const gallery = createAndAppend("gallery", document.body, {class:"gallery"})
+    const segment = document.createElement('segment');
+    const wow = document.createElement('img');
+    wow.src = "./images/image.png"
+    const caption = document.createElement('p');
+    caption.innerHTML= "Inner Beauty"
+
+    const segment2 = document.createElement('segment');
+    const wow2 = document.createElement('img');
+    wow2.src = "./images/sddefault (1).jpg"
+    const caption2 = document.createElement('p');
+    caption2.innerHTML= "Outer Beauty"
+
+    const segment3 = document.createElement('segment');
+    const wow3 = document.createElement('img');
+    wow3.src = "./images/undertale.jpg"
+    const caption3 = document.createElement('p');
+    caption3.innerHTML= "The Cast"
+
+    const segment4 = document.createElement('segment');
+    const wow4 = document.createElement('img');
+    wow4.src = "./images/beginning.png"
+    const caption4 = document.createElement('p');
+    caption4.innerHTML= "The Beginning"
+    
+    segment.appendChild(wow);
+    segment.appendChild(caption);
+    segment2.appendChild(wow2);
+    segment2.appendChild(caption2);
+    segment3.appendChild(wow3);
+    segment3.appendChild(caption3);
+    segment4.appendChild(wow4);
+    segment4.appendChild(caption4);
+    gallery.appendChild(segment)
+    gallery.appendChild(segment2)
+    gallery.appendChild(segment3)
+    gallery.appendChild(segment4)
+;
+
+const footer = document.createElement('footer');
+    footer.innerHTML = "<p> Toby Fox</p>";
+const footer2 = document.createElement('p');
+    footer2.innerHTML = "<p> Copyright @2025</p>";
+const footer3 = document.createElement('p');
+    footer3.innerHTML = "<p> Annoying Dog</p>";
+document.body.appendChild(footer);
+footer.appendChild(footer2);
+footer.appendChild(footer3);
 
 const style = `
     *{
         margin: 0;
         color: white;
+        font-size: 40px;
+        font-family: "Determination"
     }
     .site-header{
         width: 100%;
         height: 14vw;
         background-color: black;
     }
+    footer{
+        margin-top:20px;
+        margin-left: 5%;
+        display: flex;
+        background-color: black;
+        width: 90%;
+        border: 4px solid white;
+        padding: 1%;
+        position: relative;
+        justify-content: space-evenly;
+        align-items: center;
+    }
     body{
-        background-color:black;
+        background-color: black;
+    }
+    .gallery{
+        margin-top:20px;
+        margin-left: 5%;
+        display: flex;
+        background-color: black;
+        width: 90%;
+        height: 100%;
+        border: 4px solid white;
+        padding: 1%;
+        position: relative;
+    }
+    segment img{
+        width: 70%;
+    }
+    .gallery segment{
+    width: 25%
+    }
+    main{
+        margin-top:20px;
+        margin-left: 5%;
+        background-color: black;
+        width: 90%;
+        height: 530px;
+        border: 4px solid white;
+        padding: 1%;
+    }
+    main img{
+        position: absolute;
+        height: 35%;
+        right: 4%;
+        top: 60%;
+        z-index: ;
     }
     .funny{
-        height: 180px;
-        width: 70px;
+        height: 100%;
+        width: 9%;
         position: relative;
-        z-index: 1;
-        right: 680px;
+        z-index: 3;
+        right: 45%;
     }
     .funny2{
         position: relative;
-        height: 180px;
-        width: 120px;
-        left: 340px;
-        z-index:1;
+        height: 100%;
+        width: 7%;
+        left: 18%;
+        z-index:3;
     }
     .undertale{
         height:100%;
@@ -107,11 +221,11 @@ const style = `
     }
     .annoying{
         transform: scaleX(-1);
-        height: 50px;
+        height: 6%;
         position:fixed;
-        left: 170px;
-        top: 35px;
-        z-index:1;
+        left: 11%;
+        top: 3%;
+        z-index:2;
     }
     navbar{
         background-color: #fff;
@@ -126,8 +240,7 @@ const style = `
         height: 90px;
         width: 80%;
         border: 4px solid white;
-        font-size: 40px;
-        font-family: "Determination"
+        z-index: 1
     }
     a:hover{
         color: yellow;
@@ -136,15 +249,15 @@ const style = `
         color: pink;
     }
     .Sleep{
-        height:45px;
+        height:5%;
         position:fixed;
-        right: 160px;
+        right: 11%;
         top: 35px;
-        z-index:1;
+        z-index:2;
     }
     @font-face {
         font-family: "Determination";
-        src: url("./determinationmonoweb-webfont.ttf");
+        src: url("./font/determinationmonoweb-webfont.ttf");
     }
 `
 createAndAppend("style",document.head,{},style)
