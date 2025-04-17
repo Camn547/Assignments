be7a2b23ee63ab6ce18186b9884a316a20bc8834
Camn547 <Camomichael1@icloud.com>
2025-04-17 16:11:04 -0700

    Update gameTracker.js

--- Game Tracker/gameTracker.js
+++ Game Tracker/gameTracker.js
@@ -1,7 +1,5 @@
-const players = ({
-    ["Daniel", new Set()],
-    ["Jayden", new Set()],
-    ["Jon", new Set()],
-    ["Nico", new Set()],
-    ["Dylan", new Set()],
-})
+const games = ["Deltarune", "R6 Siege" ,"Fortnite" ,"Stardew Valley" ,"The Binding Of Isaac" ,"Marvel Rivals" ,"Minecraft" ,"Subnautica" ,"The Forest" ,"Dead By Daylight", "Inscryption", "Grounded", "Left 4 Dead 2", "Phasmaphobia", "Elden Ring", "R.E.P.O"]
+
+const players = new Map([
+    
+])
